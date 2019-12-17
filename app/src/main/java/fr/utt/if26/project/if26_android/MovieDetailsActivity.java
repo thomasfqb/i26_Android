588338288290f678764b0fa6cf75c8bbef531325
if26 <if26@mac02-m102.utt.fr>
2019-12-17 15:26:19 +0100

    Add Click listener and start mmovieDetailsActivity

---
+++ app/src/main/java/fr/utt/if26/project/if26_android/MovieDetailsActivity.java
@@ -0,0 +1,14 @@
+package fr.utt.if26.project.if26_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+    }
+}
